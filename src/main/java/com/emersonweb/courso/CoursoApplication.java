65372afe27ce3432bd8931968c80a76c98b6d19f
EmersondeAquino <emersonde.a.s.a.s@gmail.com>
2024-04-24 19:23:03 -0300

    Initial commit

---
+++ src/main/java/com/emersonweb/courso/CoursoApplication.java
@@ -0,0 +1,13 @@
+package com.emersonweb.courso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emersonweb/courso/CoursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emersonweb.courso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
