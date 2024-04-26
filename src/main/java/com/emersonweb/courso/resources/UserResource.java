9d59891d6baf0bede030c75ac27fb3d1dcdebfc5
EmersondeAquino <emersonde.a.s.a.s@gmail.com>
2024-04-26 16:37:45 -0300

    User entity and resource

---
+++ src/main/java/com/emersonweb/courso/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.emersonweb.courso.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.emersonweb.courso.entites.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Maria", "maria@gmail.com", "99999", "4512");
+		return ResponseEntity.ok().body(u);
+	}
+}
