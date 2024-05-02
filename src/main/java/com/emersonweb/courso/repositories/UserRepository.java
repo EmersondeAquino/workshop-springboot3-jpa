7abf4d2c8a431ad40d34ac18e4f65130765af46c
EmersondeAquino <emersonde.a.s.a.s@gmail.com>
2024-05-02 12:34:55 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/emersonweb/courso/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.emersonweb.courso.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.emersonweb.courso.entites.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	
+}
